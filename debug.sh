b94d4ffc3bacbbf3445d2a2fe3033e82fa201734
Jackson Massey <jackmassey2000@gmail.com>
2023-11-07 16:21:29 -0600

    created a script to launch during development

---
+++ debug.sh
@@ -0,0 +1,4 @@
+#Launches the wheniwork.py file as a script from the directory it is in, this emulates how the library behaves when its actually installed.
+#This was used for development purposes, ignore it if you are just trying to install the library.
+cd ./src/wiw-py/
+python3 wheniwork.py
